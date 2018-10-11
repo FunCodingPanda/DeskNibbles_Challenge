dbbc472bff484e2f9a75ec0eccfd2c83f0394815
Sophie <sophie4ngo@gmail.com>
2018-10-10 23:50:57 -0400

    Completed the Challenge

---
+++ DeskNibblesSolution.js
@@ -0,0 +1,48 @@
+const fs = require('fs');
+
+const solve = (products, snackers) => {
+  console.log('Question 1\n');
+  const fave_snacks = new Set();
+  for (let i = 0; i < snackers.length; i++) {
+    const snacker = snackers[i];
+    fave_snacks.add(snacker.fave_snack);
+  }
+  const product_names = new Set();
+  for (let i = 0; i < products.length; i++) {
+    const product = products[i];
+    product_names.add(product.title);
+  }
+  const intersection = new Set([...fave_snacks].filter(x => product_names.has(x)));
+  [...intersection].map(snack => console.log(snack));
+
+  console.log('\nQuestion 2\n');
+  const snackers_with_fave = [];
+  for (let i = 0; i < snackers.length; i++) {
+    const snacker = snackers[i];
+    if (intersection.has(snacker.fave_snack)) {
+      snackers_with_fave.push(snacker);
+      console.log(snacker.email);
+    }
+  }
+
+  console.log('\nQuestion 3\n');
+  let total_cost = 0.0;
+  const price_mapping = {};
+  for (let i = 0; i < products.length; i++) {
+    const product = products[i];
+    price_mapping[product.title] = parseFloat(product.variants[0].price);
+  }
+  for (let i = 0; i < snackers_with_fave.length; i++) {
+    const snacker = snackers_with_fave[i];
+    total_cost += price_mapping[snacker.fave_snack];
+  }
+  console.log(`$${total_cost}`);
+};
+
+fs.readFile('products.json', 'utf8', (err, data) => {
+  const products = JSON.parse(data)['products'];
+  fs.readFile('MOCK_SNACKER_DATA.json', 'utf8', (err, data) => {
+    const snackers = JSON.parse(data);
+    solve(products, snackers);
+  });
+});
--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # DeskNibbles_Challenge
+
+## Step 1. Solve this:
+
+The snacker's list is MOCK_SNACKER_DATA.json
+
+Find all emails of snackers with a 'fave_snack'of a product we stock:
+
+The product list is products.json 
+
+a) List the real stocked snacks you found under the snacker's 'fave_snack'?
+b) What're the emails of the snackers who listed those as a 'fave_snack'?
+c) If all those snackers we're to pay for their 'fave_snack'what's the total price?
+
+## Step 2. 
+My favourite snack is pistashios. 
+
+## Step 3. 
+run node node DeskNibblesSolution.js
